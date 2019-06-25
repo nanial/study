092f3fdea3a7f54f5693d8e641b7eea44d6a7d8f
nanial <nanial@mail.ru>
2019-06-25 11:37:22 +0300

    epam

---
+++ src/com/task/second/arrayOne/ArraysNine.java
@@ -0,0 +1,42 @@
+package com.task.second.arrayOne;
+//В массиве целых чисел с количеством элементов n найти
+// наиболее часто встречающееся число. Если таких
+//чисел несколько, то определить наименьшее из них.
+
+class ArraysNine {
+
+    public static void main(String[] args){
+        int n = 20; // for example
+        int [] arr = new int[n];// init array
+        int [] popularNum = new int[arr.length];//array of popular number
+        int valOfRes = arr [0];
+        int minOfMostRes = popularNum [0];
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = (int) (Math.random() * 10 - 5);
+            System.out.print(arr[i] + "  ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (Integer j : arr) {
+                if (arr[i] == j) {
+                    popularNum [i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < popularNum.length; i++) {
+            if (popularNum[i] > minOfMostRes) {
+                minOfMostRes = popularNum[i];
+                valOfRes = arr[i];
+            }
+            if ((minOfMostRes == popularNum[i]) && (arr[i] < valOfRes)) {
+                valOfRes = arr[i];
+            }
+        }
+        System.out.println(valOfRes);
+    }
+}
+
