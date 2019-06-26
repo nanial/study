fd94795da8538209fb43d4c5ee99fe9c061e68ae
nanial <nanial@mail.ru>
2019-06-26 13:19:50 +0300

    epam

---
+++ src/com/task/second/arrayOfArray/ArraysOfArraysEight.java
@@ -0,0 +1,52 @@
+package com.task.second.arrayOfArray;
+
+//В числовой матрице поменять местами два столбца любых столбца,
+// т. е. все элементы одного столбца поставить на соответствующие
+// им позиции другого, а его элементы второго переместить в первый.
+// Номера столбцов вводит пользователь с клавиатуры
+
+import java.util.Scanner;
+
+public class ArraysOfArraysEight {
+
+    public static void main(String[] args) {
+
+        int n = 5;//for   example
+        int m = 6;
+        int from;
+        int to;
+        int temp;
+        int[][] arr = new int [n][m];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                arr[i][j] = (int) (Math.random() * 100 - 50);
+                System.out.format("%5d", arr[i][j]);
+            }
+            System.out.println();
+        }
+        try(Scanner scan = new Scanner(System.in)) {
+            System.out.println("Insert number of column 'from' number contains[0;5] :");
+            from = scan.nextInt();
+            System.out.println("Insert number of column 'to' number contains[0;5] :");
+            to = scan.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            temp = arr[i][from];
+            arr[i][from] = arr[i][to];
+            arr[i][to] = temp;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.format("%5d", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
