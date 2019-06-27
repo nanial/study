963dc71a9aa38bd8a51371078eac330129fdc6c6
nanial <nanial@mail.ru>
2019-06-27 09:45:16 +0300

    epam

--- src/com/task/second/arrayOfArray/ArraysOfArraysSeven.java
+++ src/com/task/second/arrayOfArray/ArraysOfArraysSeven.java
@@ -15,16 +15,17 @@ public class ArraysOfArraysSeven {
         int counter = 0;
         double [][] arr = new double [n][n];
 
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < arr.length; i++) {
 
-            for(int j = 0; j < arr.length; j++){
-                    arr[i][j] = Math.sin((Math.pow(i , 2) -
-                            Math.pow(j , 2) / arr.length));
-                if (arr[i][j] > 0){
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = Math.sin((Math.pow(i, 2) -
+                        Math.pow(j, 2) / arr.length));
+
+                if (arr[i][j] > 0) {
                     counter++;
                 }
             }
-            System.out.println(Arrays.toString(arr[i]));
+           System.out.println(Arrays.toString(arr[i]));
         }
         System.out.println("Number of positive member is " + counter);
     }
