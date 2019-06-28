4f6f6c1482cf8e0e72c872c1edb14ba36c175487
nanial <nanial@mail.ru>
2019-06-28 05:52:42 +0300

    epam

---
+++ src/com/task/second/arrayOfArray/ArraysOfArraysThirteen.java
@@ -0,0 +1,42 @@
+package com.task.second.arrayOfArray;
+
+//Отсортировать строки матрицы по убыванию значений элементов
+
+public class ArraysOfArraysThirteen {
+    public static void main(String[] args) {
+
+        int n = 10;//for example
+        int m = 10;
+        int temp;
+        int[][] arr = new int [n][m];
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                arr[i][j] = (int) (Math.random() * 16);
+                System.out.format("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("-------------------------------");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (var v: arr) {
+                for (int j = 1; j < arr[i].length; j++) {
+                    if (arr[i][j] > arr[i][j - 1]) {
+                        temp = arr[i][j];
+                        arr[i][j] = arr[i][j - 1];
+                        arr[i][j - 1] = temp;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.format("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
