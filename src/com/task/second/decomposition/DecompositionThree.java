faec18f4d2c6c02fbb3d2dc3d73a55bf82ed3006
nanial <nanial@mail.ru>
2019-07-08 11:38:24 +0300

    epam

---
+++ src/com/task/second/decomposition/DecompositionThree.java
@@ -0,0 +1,25 @@
+package com.task.second.decomposition;
+
+//Вычислить площадь правильного шестиугольника со стороной а,
+// используя метод вычисления площади треугольника.
+
+public class DecompositionThree {
+
+        public static void main(String[] args) {
+            System.out.println(hexagonArea(5));
+        }
+
+        private static double triangleArea(int a) {
+            return (a * a * Math.sqrt(3)) / 4;
+        }
+
+        private static double hexagonArea(int a){
+            return 6 * triangleArea(a);
+        }
+
+}
+
+
+
+
+
