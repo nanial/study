b794a1cb5452383825fe5165104c8236f7e1344a
nanial <nanial@mail.ru>
2019-07-15 15:18:13 +0300

    epam

---
+++ src/com/task/third/StringAndStrBuilder/StringAndStrBuilderFive.java
@@ -0,0 +1,26 @@
+package com.task.third.StringAndStrBuilder;
+
+//Подсчитать, сколько раз среди символов заданной строки встречается буква “а”
+
+
+public class StringAndStrBuilderFive {
+
+    public static void main(String[] args) {
+        String s = "You know, it often is so " +
+                "those simple, old-fashioned remedies are sometimes" +
+                " more efficacious than all the dispensary stuff";
+
+        System.out.println(countOfA(s));
+
+    }
+    private static int countOfA(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 97) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
