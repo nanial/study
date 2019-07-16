25e5b2b1dfe779404025f162679ddf09830b512c
nanial <nanial@mail.ru>
2019-07-16 13:32:42 +0300

    epam

---
+++ src/com/task/third/StringAndStrBuilder/StringAndStrBuilderEight.java
@@ -0,0 +1,25 @@
+package com.task.third.StringAndStrBuilder;
+
+//Вводится строка слов, разделенных пробелами. Найти самое длинное
+// слово и вывести его на экран. Случай, когда самых длинных слов
+// может быть несколько, не обрабатывать
+
+public class StringAndStrBuilderEight {
+
+    public static void main(String[] args) {
+
+        String s = "And they didn't give me pills; they gave me" +
+                " clumps on the side of the head";
+
+        String[] word= s.split(" ");
+        String maxWord=" ";
+
+        for(var i :  word){
+
+            if(i.length() >= maxWord.length()){
+                maxWord= i;
+            }
+        }
+        System.out.println(maxWord);
+    }
+}
