57acde3a8b42e88c1c295570038cde3230e83c7d
nanial <nanial@mail.ru>
2019-07-16 13:19:26 +0300

    epam

---
+++ src/com/task/third/StringAndStrBuilder/StringAndStrBuilderSeven.java
@@ -0,0 +1,20 @@
+package com.task.third.StringAndStrBuilder;
+
+//Вводится строка. Требуется удалить из нее повторяющиеся символы
+// и все пробелы. Например, если было введено "abc cde def",
+// то должно быть выведено "abcdef"
+
+public class StringAndStrBuilderSeven {
+
+    public static void main(String[] args) {
+
+        String s = "And they didn't give me pills; they gave me" +
+                " clumps on the side of the head";
+        StringBuilder sb = new StringBuilder();
+
+        s.chars().distinct().forEach(c -> sb.append((char) c));
+        System.out.println("'" + sb.toString().replaceAll("\\s+", "") + "'");
+
+    }
+
+}
