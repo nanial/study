aa94b8ee0b6be8e2848a61800ecbd892085d0cd8
nanial <nanial@mail.ru>
2019-07-25 22:54:38 +0300

    epam

--- src/com/task/fourth/simpleClass/Triangle.java
+++ src/com/task/fourth/simpleClass/Triangle.java
@@ -42,10 +42,15 @@ public class Triangle {
 
     private void medianPoint(int x1, int y1, int x2, int y2, int x3, int y3){
 
-        double medianPointX = (x1 + x2 + x3) / 3;
-        double medianPointY = (y1 + y2 + y3) / 3;
+        if(((x1-x3)/(x2-x3)) != ((y1-y3)/(y2-y3))){
 
-        System.out.println(medianPointX + "," + medianPointY);
+            int medianPointX = (x1 + x2 + x3) / 3;
+            int medianPointY = (y1 + y2 + y3) / 3;
+            System.out.println("(" + medianPointX + "," + medianPointY + ")");
+        }
+        else {
+            System.out.println("The points are collinear");
+        }
     }
 
     public static void main(String[] args) {
