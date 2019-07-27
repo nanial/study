8389cc5882ea58e4562c1593e6a8bb0d8646319f
nanial <nanial@mail.ru>
2019-07-27 07:05:36 +0300

    epam

--- src/com/task/fourth/simpleClass/Customer.java
+++ src/com/task/fourth/simpleClass/Customer.java
@@ -9,6 +9,7 @@ package com.task.fourth.simpleClass;
 //b) список покупателей, у которых номер кредитной карточки находится в заданном интервале
 
 import java.math.BigInteger;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Customer {
@@ -113,21 +114,80 @@ public class Customer {
 }
 class CustomerRealization{
 
-    static Customer []  arrCustomer = new Customer[]{
+    private static Customer []  arrCustomer = new Customer[]{
 
-            new Customer("Gorin", "Nikolay", "Semenovich", "321456791BY3074"),
-            new Customer("Molochov", "Anton", "Pavlovich", "321456791BY1109"),
+            new Customer("Gorin", "Nikolay", "Semenovich",
+                    new BigInteger("321456794003215"),"321456791BY3074"),
+            new Customer("Molochov", "Anton", "Pavlovich",
+                    new BigInteger("321456791003215"),"321456791BY1109"),
             new Customer("Alechin", "Boris", "Lvovich",
                     new BigInteger("321456791001456"), "321456791BY1456"),
-            new Customer("Terechin", "Maxim", "Maximych", "321456791BY1459"),
+            new Customer("Terechin", "Maxim", "Maximych",
+                    new BigInteger("321456791011215"),"321456791BY1459"),
             new Customer("Malyscheva", "Olga", "Kuzminicna",
                     new BigInteger("321456791003954"), "321456791BY3954"),
             new Customer("Alexeev", "Vulf", "Gerdovich",
-                    new BigInteger("321456791003215"), "321456791BY3215"),
-            new Customer("Grigoryan", "Anna", "Yosypovna", "321456791BY3478")
+                    new BigInteger("321456791030615"), "321456791BY3215"),
+            new Customer("Grigoryan", "Anna", "Yosypovna",
+                    new BigInteger("321456793003210"),"321456791BY3478")
 
     };
 
+    private static void choiceCustomers(Customer[] arrCustomer){
+
+        try(@SuppressWarnings("") Scanner scan = new Scanner(System.in)) {
+            System.out.println("Input '1' for get names of array," +
+                    "'2' for surnames ,'3' for patronymic " +
+                    "'4' for number of account, '5' for number of credit card ");
+
+            while (!scan.hasNextInt()) {
+                scan.next();
+                System.out.println("Input 1/2/3/4/5");
+            }
+
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1:
+                    for (var v : arrCustomer) {
+                        System.out.println(v.getName());
+                    }
+                    break;
+                case 2:
+                    for (var v : arrCustomer) {
+                        System.out.println(v.getSurname());
+                    }
+                    break;
+                case 3:
+                    for (var v : arrCustomer) {
+                        System.out.println(v.getPatronymic());
+                    }
+                    break;
+                case 4:
+                    for (var v : arrCustomer) {
+                        System.out.println(v.getAccount());
+                    }
+                    break;
+                case 5:
+                    for (var v : arrCustomer) {
+                        System.out.println(v.getNumOfCreditCard());
+                    }
+                    break;
+                default:
+                    System.out.println("You have been insert incorrect command");
+                    break;
+            }
+        }
+
+    }
+    private static void certainNumOfCred(Customer[] arrCustomer){
+
+        for (var v: arrCustomer) {
+            if((v.getNumOfCreditCard().compareTo(new BigInteger("321456791000000")) > 0) &&
+                    (v.getNumOfCreditCard().compareTo(new BigInteger("321456791999999")) < 0))
+            System.out.println(v.toString());
+        }
+    }
     private static void showCustomers(Customer[] arrCustomer){
 
         for (var v: arrCustomer) {
@@ -167,9 +227,13 @@ class CustomerRealization{
     }
 
     public static void main(String[] args) {
-       /* showCustomers(arrCustomer);
-        System.out.println();*/
+        showCustomers(arrCustomer);
+        System.out.println();
         showCustomers(sortOfSurname());
+        System.out.println();
+        choiceCustomers(arrCustomer);
+        System.out.println();
+        certainNumOfCred(arrCustomer);
     }
 }
 
