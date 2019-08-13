58a74097de0f9f893d43cbac342ce3c41754145b
nanial <37077622+nanial@users.noreply.github.com>
2019-08-13 17:57:08 +0200

    Update Main.java

--- src/com/task/fifth/oopCalendar/Main.java
+++ src/com/task/fifth/oopCalendar/Main.java
@@ -10,25 +10,7 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-       /* Date date = new Date(0);
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM");
-        System.out.println(format.format(date));//return 01/01
-        Date indep = new Date();
-
-        String s = "03/07";
-        try {
-            indep = format.parse(s);//parse 3 july
-        }
-        catch (ParseException pe){}
-
-        System.out.println(indep.toString());
-
-        System.out.println(new Calendar.Year(2019).getEasterDate(2019));
-        System.out.println(new Calendar.Year(2019).ortodoxPasqua(2019));
-        new Calendar().printConst();
-        System.out.println(new Calendar.Year(2019).dayOfMemory(2019));*/
-       new Calendar.Year(2019).getWeekEnds();
-
+      
     }
 
 }
