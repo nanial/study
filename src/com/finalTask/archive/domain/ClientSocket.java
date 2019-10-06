7ab5213510c67e0d71833f026a7705733baa43bb
nanial <nanial@mail.ru>
2019-10-06 17:22:02 +0300

    epam

--- src/com/finalTask/archive/domain/ClientSocket.java
+++ src/com/finalTask/archive/domain/ClientSocket.java
@@ -1,69 +1,12 @@
 package com.finalTask.archive.domain;
 
-import com.finalTask.archive.apiBusiness.ArchiveManager;
-import com.finalTask.archive.apiDao.ArchiveDao;
-import com.finalTask.archive.business.ArchiveManagerImpl;
-import com.finalTask.archive.dao.ArchiveDaoImpl;
-import com.finalTask.archive.filter.Filter;
-
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class ClientSocket {
 
-    static ArrayList<Customer> customers = new Customers().fillListOfCustomer();
-
-    public static void userService() {
-
-    }
-
-    public static ArrayList<Portfolio> adminPanel(ArrayList<Portfolio> portfolios) {
-
-
-        ArchiveDao archD = new ArchiveDaoImpl();
-        ArchiveManager am = new ArchiveManagerImpl(archD);
-
-      /*  try (@SuppressWarnings("")
-             Scanner scan = new Scanner(System.in)) {
-
-            System.out.println("Enter for admin");
-            String position = scan.nextLine();
-
-            for (Customer c : customers) {
-
-                if (c.getPosition().equals(position)) {
-
-                    if (c.getRole() == Role.ADMIN) {*/
-
-        //scanner with switch
-
-        am.updatePortfolio(portfolios,
-                1, "Geography",//for example
-                45, 9.5);
-
-        Portfolio portfolio = am.createPortfolio("Fiodor",
-                "Mikhoylov", 62,
-                "Archaeology", 2.6);
-        portfolio.setIdOfStudent(portfolios.size() + 1);
-        portfolios.add(portfolio);
-
-        am.searchCertainPortfolio(portfolios, new Filter(0, "Geography"));
-        //end switch
-                       /* for (Portfolio p : portfolios) {
-
-                            System.out.println(p.toString());
-                        }
-
-                    }
-                }
-
-            }
-
-        }*/
-        return portfolios;
-    }
 
     public static void main(String[] args) {
 
@@ -88,26 +31,10 @@ public class ClientSocket {
                     cnf.getMessage();
             }
 
+            Menu menu = new Menu();
+            menu.login(portfolios);
 
-            for (Portfolio p : portfolios) {
-
-                System.out.println(p.toString());
-            }
-            System.out.println("///////////////////////////");
-
-            adminPanel(portfolios);
-
-            System.out.println("//////////////////////");
-
-            for (Portfolio p : portfolios) {
-
-                System.out.println(p.toString());
-            }
-
-            portfolios = adminPanel(portfolios);
-
-
-            oos.writeObject(portfolios);
+            oos.writeObject(menu.returnPortfolios);
 
         } catch (IOException io) {
 
--- src/com/finalTask/archive/domain/Menu.java
+++ src/com/finalTask/archive/domain/Menu.java
@@ -1,11 +1,9 @@
 package com.finalTask.archive.domain;
 
 import com.finalTask.archive.apiBusiness.ArchiveManager;
-import com.finalTask.archive.business.ArchiveBuilderFactory;
 import com.finalTask.archive.business.ArchiveManagerImpl;
 import com.finalTask.archive.dao.ArchiveDaoImpl;
 import com.finalTask.archive.filter.Filter;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,12 +12,11 @@ public class Menu {
 
     ArchiveManager am = new ArchiveManagerImpl(new ArchiveDaoImpl());
 
-    Archive arch = new ArchiveBuilderFactory().getArchiveBuilder().getArchive();
     ArrayList<Customer> customers = new Customers().fillListOfCustomer();
+    ArrayList<Portfolio> returnPortfolios = new ArrayList<>();
 
 
-
-    public void login() {
+    public void login(ArrayList<Portfolio> portfolios) {
 
         try (@SuppressWarnings("")
              Scanner scan = new Scanner(System.in)) {
@@ -38,13 +35,13 @@ public class Menu {
 
                     if (c.getRole() == Role.USER){
 
-                        System.out.println("Welcome");
-                      //  this.userService();
+                        System.out.println("Welcome, " + position);
+                        this.userService(portfolios);
                     }
                     if (c.getRole() == Role.ADMIN){
 
                         System.out.println("Hello, admin");
-                        this.adminPanel();
+                        this.adminPanel(portfolios);
                     }
                 }
 
@@ -55,13 +52,13 @@ public class Menu {
     public void userService(ArrayList<Portfolio> portfolios) {
 
         System.out.println("Input 1 for list of portfolios, 2 for search certain portfolio per lastName of student," +
-                " 3 for search portfolio(s) of certain group, 4 for search portfolio(s) of certain group," +
+                " 3 for search portfolio(s) of certain group, 4 for search portfolio(s) of certain department," +
                 " 5 for escape");
         try (@SuppressWarnings("")
              Scanner scan = new Scanner(System.in)) {
             int choice = scan.nextInt();
 
-            while (true) {
+         one:   while (true) {
 
                 switch (choice) {
 
@@ -106,7 +103,8 @@ public class Menu {
 
                     case 5:
                         System.out.println("See you later..");
-                        System.exit(0);
+                        returnPortfolios.addAll(portfolios);
+                        break one;
 
                     default:
                         System.out.println("You have been incorrect command");
@@ -117,86 +115,115 @@ public class Menu {
 
         }
 
-    }
-    public void adminPanel(){
 
-        System.out.println("Input 1 for update book, 2 for delete book," +
-                " 3 for add books, 4 for list per page of book, 5 for escape");
+    }
+    public void adminPanel(ArrayList<Portfolio> portfolios){
+
+
+        System.out.println("Input 1 for list of portfolios, 2 for search certain portfolio per lastName of student," +
+                " 3 for search portfolio(s) of certain group, 4 for search portfolio(s) of certain department," +
+                "5 for create new portfolio, 6 for update portfolio, 7 for escape");
+
         try (@SuppressWarnings("")
              Scanner scan = new Scanner(System.in)) {
             int choice = scan.nextInt();
 
-            while (true) {
+           one: while (true) {
 
                 switch (choice) {
 
                     case 1:
 
-                        System.out.println("Enter id of book");
-                        int bookId = new Scanner(System.in).nextInt();
-                        System.out.println("It is digital? (true/false)");
-                        boolean isDigital =  new Scanner(System.in).nextBoolean();
-                        System.out.println("Write the description of book, please");
-                        String description =  new Scanner(System.in).nextLine();
-                       // bm.writeBooksInFile(arch.updateBook(bookId, isDigital, description));
-                        System.out.println("Choice next operation");
-                        choice = scan.nextInt();
-                        break;
-
-                    case 2:
-
-                        System.out.println("Enter id of book");
-                        int bId = new Scanner(System.in).nextInt();
-                       // bm.writeBooksInFile(arch.delBook(bId));
-                        System.out.println("Choice next operation");
-                        choice = scan.nextInt();
-                        break;
-
-                    case 3:
-
-                        System.out.println("Input author of book, please");
-                        String author =  new Scanner(System.in).nextLine();
-                        System.out.println("Input title of book, please");
-                        String title =  new Scanner(System.in).nextLine();
-                        System.out.println("It is digital? (true/false)");
-                        boolean isDigit =  new Scanner(System.in).nextBoolean();
-                        System.out.println("Write the description of book, please");
-                        String descript =  new Scanner(System.in).nextLine();
+                        portfolios.forEach(System.out::println);
                         System.out.println("Choice next operation");
 
-                      //  Book newBook = new Book(author, title, isDigit, descript);
-                        //bm.writeBooksInFile(arch.fillCatalog(newBook));
-
-                        for (Customer c : customers) {
-                          //  am.sendEmail(c, newBook );
-                            //c.getInBox().forEach(System.out::println);
-                        }
-                        choice = scan.nextInt();
-                        break;
-
-                    case 4:
-
-                        System.out.println("Enter number of page");
-                       // bm.getBookList(new BookFilter(new Scanner(System.in).nextInt()));
-                        System.out.println("Choice next operation");
                         while (!scan.hasNextInt()){
+
                             System.out.println("Choice next operation");
                             scan.next();
                         }
                         choice = scan.nextInt();
                         break;
 
+                    case 2:
+
+                        System.out.println("Input lastName of student, please");
+                        String lastName = new Scanner(System.in).nextLine();
+                        am.searchCertainPortfolio(portfolios, new Filter(lastName));
+                        System.out.println("Choice next operation, please");
+                        choice = scan.nextInt();
+                        break;
+
+                    case 3:
+
+                        System.out.println("Input number of group, please");
+                        int numOfGroup =  new Scanner(System.in).nextInt();
+                        am.searchCertainPortfolio(portfolios, new Filter(numOfGroup, null));
+                        System.out.println("Choice next operation, please");
+                        choice = scan.nextInt();
+                        break;
+
+                    case 4:
+                        System.out.println("Input department, please");
+                        String  department =  new Scanner(System.in).nextLine();
+                        am.searchCertainPortfolio(portfolios, new Filter(0, department));
+                        System.out.println("Choice next operation, please");
+                        choice = scan.nextInt();
+                        break;
+
                     case 5:
+
+                        System.out.println("Input name, please");
+                        String  newName =  new Scanner(System.in).nextLine();
+                        System.out.println("Input lastName, please");
+                        String  newLastName =  new Scanner(System.in).nextLine();
+                        System.out.println("Input number of group, please");
+                        int number =  new Scanner(System.in).nextInt();
+                        System.out.println("Input department, please");
+                        String  dep =  new Scanner(System.in).nextLine();
+                        System.out.println("Input average score, please");
+                        double score =  new Scanner(System.in).nextDouble();
+
+                        Portfolio portfolio = am.createPortfolio(newName,
+                                newLastName, number, dep, score);
+                        portfolio.setIdOfStudent(portfolios.size() + 1);
+                        portfolios.add(portfolio);
+
+                        System.out.println("Choice next operation, please");
+                        choice = scan.nextInt();
+
+                        break;
+
+                    case 6:
+
+                        System.out.println("Input id of student, please");
+                        int id =  new Scanner(System.in).nextInt();
+                        System.out.println("Input number of group, please");
+                        int numGr =  new Scanner(System.in).nextInt();
+                        System.out.println("Input department, please");
+                        String  depart =  new Scanner(System.in).nextLine();
+                        System.out.println("Input average score, please");
+                        double avScore =  new Scanner(System.in).nextDouble();
+
+                        am.updatePortfolio(portfolios,id, depart, numGr, avScore);
+                        System.out.println("Choice next operation, please");
+                        choice = scan.nextInt();
+
+                        break;
+
+                    case 7:
                         System.out.println("See you later..");
-                        System.exit(0);
+                        returnPortfolios.addAll(portfolios);
+                        break one;
 
                     default:
                         System.out.println("You have been incorrect command");
                         System.out.println("Choice next operation");
                         choice = scan.nextInt();
-
                 }
             }
+
         }
+
     }
 }
