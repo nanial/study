009abe09c2dd57a203b72e386ecc74a6e7e7ff56
nanial <nanial@mail.ru>
2019-11-17 20:17:00 +0300

    epam

--- src/com/task/second/array_linear/ArraysNine.java
+++ src/com/task/second/array_linear/ArraysNine.java
@@ -3,9 +3,12 @@ package com.task.second.array_linear;
 // наиболее часто встречающееся число. Если таких
 //чисел несколько, то определить наименьшее из них.
 
+import java.util.Arrays;
+
 class ArraysNine {
 
     public static void main(String[] args){
+
         int n = 20; // for example
         int [] arr = new int[n];// init array
         int [] popularNum = new int[arr.length];//array of popular number
@@ -13,6 +16,7 @@ class ArraysNine {
         int minOfMostRes = popularNum [0];
 
         for (int i = 0; i < arr.length; i++){
+
             arr[i] = (int) (Math.random() * 10 - 5);
             System.out.print(arr[i] + "  ");
         }
@@ -20,22 +24,30 @@ class ArraysNine {
         System.out.println();
 
         for (int i = 0; i < arr.length; i++) {
+
             for (Integer j : arr) {
+
                 if (arr[i] == j) {
+
                     popularNum [i]++;
                 }
             }
+
         }
 
         for (int i = 0; i < popularNum.length; i++) {
+
             if (popularNum[i] > minOfMostRes) {
+
                 minOfMostRes = popularNum[i];
                 valOfRes = arr[i];
             }
             if ((minOfMostRes == popularNum[i]) && (arr[i] < valOfRes)) {
+
                 valOfRes = arr[i];
             }
         }
+
         System.out.println(valOfRes);
     }
 }
