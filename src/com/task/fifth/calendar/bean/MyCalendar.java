de88ad83ebd7647c10aec0c7c7fab69730c0a011
nanial <nanial@mail.ru>
2019-11-20 17:54:39 +0300

    epam

--- src/com/task/fifth/calendar/bean/MyCalendar.java
+++ src/com/task/fifth/calendar/bean/MyCalendar.java
@@ -1,6 +1,6 @@
 package com.task.fifth.calendar.bean;
 
-import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 public class MyCalendar {
 
@@ -8,8 +8,6 @@ public class MyCalendar {
             "08/03", "01/05", "09/05", "03/07", "07/11", "25/12"};
 
     private int numOfYear;
-
-    SimpleDateFormat format = new SimpleDateFormat("dd/MM");
 
     public MyCalendar(int numOfYear) {
         this.numOfYear = numOfYear;
@@ -27,10 +25,40 @@ public class MyCalendar {
         this.numOfYear = numOfYear;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MyCalendar)) return false;
+
+        MyCalendar that = (MyCalendar) o;
+
+        if (getNumOfYear() != that.getNumOfYear()) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(getConstantHolidays(), that.getConstantHolidays());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(getConstantHolidays());
+        result = 31 * result + getNumOfYear();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MyCalendar{" +
+                "constantHolidays=" + Arrays.toString(constantHolidays) +
+                ", numOfYear=" + numOfYear +
+                '}';
+    }
+
     public class TransientHolidays {
 
         public TransientHolidays(int numOfYear) {
             MyCalendar.this.numOfYear = numOfYear;
+        }
+
+        public TransientHolidays() {
         }
 
     }
